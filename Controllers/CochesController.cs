6545373fbee872890f473edb9029558c082a6980
ElliotSolano <37352277+ElliotSolano@users.noreply.github.com>
2018-03-14 00:31:10 +0100

    Add files via upload

---
+++ Controllers/CochesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    
+    public class CochesController : ApiController
+    {
+        List<Coche> listaCoches = new List<Coche>();
+        public CochesController()
+        {
+            Coche c = new Coche { idCoche = 1, Marca = "Ferrari", Modelo = "F40" };
+            listaCoches.Add(c);
+
+            c = new Coche { idCoche = 2, Marca = "Ford", Modelo = "GT500" };
+            listaCoches.Add(c);
+
+            c = new Coche { idCoche = 3, Marca = "Toyota", Modelo = "Supra" };
+            listaCoches.Add(c);
+        }
+
+        public List<Coche> GetCoches()
+        {
+            return listaCoches;
+        }
+
+        public Coche GetCoche(int id)
+        {
+            return listaCoches.Where(t => t.idCoche == id).FirstOrDefault();
+        }
+    }
+}
---
+++ Models/Coche.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Coche
+    {
+        public int idCoche { get; set; }
+        public String Marca { get; set; }
+        public String Modelo { get; set; }
+    }
+}
